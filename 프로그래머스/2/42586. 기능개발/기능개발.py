1ea4bcb6453698027f3140496033db1a189cd4c3
bohyeong <95080710+jbh010204@users.noreply.github.com>
2025-03-20 23:58:41 +0900

    [level 2] Title: 기능개발, Time: 0.68 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -8,19 +8,23 @@ def solution(progresses, speeds):
     
     pushed = [False for i in range(len(progresses))]
     print(pushed)
-    while idx != len(speeds):
+    for k in range(10):
         for i, item in enumerate(progresses):
+            print(pushed)
             if pushed[i] == False:
                 progresses[i] += speeds[i]
                 if(progresses[i] >= 100):
+                    print("i입니다", i)
                     dev.append(i)
                     pushed[i] = True
                     
         ct = 0
+        print("dev!!!!!!!!", dev)
         while True:
             # 만약 지금 배포가능성 있다면
             if idx in dev:
                 dev.remove(idx)
+                print("pop!!", idx)
                 idx += 1
                 ct += 1
             else:
@@ -28,7 +32,10 @@ def solution(progresses, speeds):
                     answer.append(ct)
                 break
             
-    
+        if(ct != 0):
+            answer.append(ct)
+        print("dev", dev)
+        print("answer", answer)
                 
         
     return answer
