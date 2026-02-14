ee517ee9c1e50ade65b593b2e6270d0a1c848da3
장보형 <95080710+jbh010204@users.noreply.github.com>
2026-02-14 21:35:18 +0900

    [level 2] Title: 양궁대회, Time: 7.24 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+class Solution {
+    // 어피치 a -> 라이언 b
+    // k (1~10) a == b 이면 어피치가 k점 가져감
+    // info[11] = [10점 맞힌 개수 ~ 0점 맞힌 개수]
+    
+//     dfs 중복 순열 
+//     -> length 도달하면 calculate 
+//         -> max값 갱신 List<> 초기화 후 다시 넣기
+//         -> max값 똑같으면 add
+        
+//     -> process() 가장 낮은 점수에 많이 쏜 사람 비교 
+//         -> index역순으로 s1 s2비교 한쪽에만 있으면 그거 리턴 둘 다 있으면 continue;
+    
+    static List<int[]> arr = new ArrayList<>();
+    static int N;
+    static int max = 0;
+    
+    public int[] solution(int n, int[] info) {
+        arr.clear();
+        max = 0;
+        N = n;
+        
+        int[] out = new int[11];
+        Arrays.fill(out, 0); 
+        
+        dfs(info, out, 0, 0);
+        
+        if(max == 0){
+            return new int[]{-1};
+        }
+        
+        return diff(arr);
+    }
+    
+    void dfs(int[] info, int[] out, int depth, int start){
+        if(depth == N){
+            calculate(info, out.clone());
+            return;
+        }
+        
+        for(int i=start; i<out.length; i++){
+            if(info[i] < out[i]){
+                continue;
+            }
+            out[i] += 1;
+            dfs(info, out, depth + 1, i);
+            out[i] -= 1;    
+        }
+        return;
+    }
+    
+    void calculate(int[] info, int[] out){
+        int lion = 0;
+        int cnt = 0;
+        for(int i=0; i<info.length; i++){
+            int s1 = info[i];
+            int s2 = out[i];
+            
+            if(s1 != 0 || s2 != 0){
+                if(s1 < s2){
+                    cnt += 10-i;
+                } else{
+                    lion += 10-i;
+                }    
+            }
+        }
+        
+        int diff = cnt - lion;
+        if(max == diff){
+            arr.add(out.clone());
+        }
+        else if(max < diff){
+            max = diff;
+            arr.clear();
+            arr.add(out.clone());
+        }
+    }
+    
+    int[] diff(List<int[]> arr){
+        int[] minList = arr.get(0);
+        
+        for(int[] list : arr){
+            for(int i=10; i>=0; i--){
+                if(minList[i] > list[i]){
+                    break;
+                } 
+                else if(minList[i] == list[i]){
+                    continue;
+                } 
+                else{
+                    minList = list.clone();       
+                    break;
+                }
+            }
+        }
+        
+        return minList;
+    }
+}
