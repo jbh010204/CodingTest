92ce3cb9c514a156158b322ede37c0531ff2ca1c
장보형 <95080710+jbh010204@users.noreply.github.com>
2026-02-14 22:15:26 +0900

    [level 2] Title: 양궁대회, Time: 4.54 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -12,13 +12,11 @@ class Solution {
         
 //     -> process() 가장 낮은 점수에 많이 쏜 사람 비교 
 //         -> index역순으로 s1 s2비교 한쪽에만 있으면 그거 리턴 둘 다 있으면 continue;
-    
-    static List<int[]> arr = new ArrayList<>();
     static int N;
     static int max = 0;
-    
+    static int[] best = new int[11];
+        
     public int[] solution(int n, int[] info) {
-        arr.clear();
         max = 0;
         N = n;
         
@@ -27,11 +25,9 @@ class Solution {
         
         dfs(info, out, 0, 0);
         
-        if(max == 0){
-            return new int[]{-1};
-        }
+        if(max == 0) return new int[]{-1};
         
-        return diff(arr);
+        return best;
     }
     
     void dfs(int[] info, int[] out, int depth, int start){
@@ -41,9 +37,8 @@ class Solution {
         }
         
         for(int i=start; i<out.length; i++){
-            if(info[i] < out[i]){
-                continue;
-            }
+            if(info[i] < out[i]) continue;
+            
             out[i] += 1;
             dfs(info, out, depth + 1, i);
             out[i] -= 1;    
@@ -52,50 +47,31 @@ class Solution {
     }
     
     void calculate(int[] info, int[] out){
-        int lion = 0;
-        int cnt = 0;
+        int lion = 0; int apeach = 0;
+        
         for(int i=0; i<info.length; i++){
             int s1 = info[i];
             int s2 = out[i];
             
             if(s1 != 0 || s2 != 0){
-                if(s1 < s2){
-                    cnt += 10-i;
-                } else{
-                    lion += 10-i;
-                }    
+                if(s1 < s2) apeach += 10-i;
+                else lion += 10-i; 
             }
         }
         
-        int diff = cnt - lion;
-        if(max == diff){
-            arr.add(out.clone());
-        }
+        int diff = apeach - lion;
+        if(max == diff && isBetter(out)) best = out.clone();
         else if(max < diff){
             max = diff;
-            arr.clear();
-            arr.add(out.clone());
-        }
+            best = out.clone();
+        } 
     }
     
-    int[] diff(List<int[]> arr){
-        int[] minList = arr.get(0);
-        
-        for(int[] list : arr){
-            for(int i=10; i>=0; i--){
-                if(minList[i] > list[i]){
-                    break;
-                } 
-                else if(minList[i] == list[i]){
-                    continue;
-                } 
-                else{
-                    minList = list.clone();       
-                    break;
-                }
-            }
+    boolean isBetter(int[] tmp){
+        for(int i=10; i>0; i--){
+            if(best[i] > tmp[i]) return false;
+            if(best[i] < tmp[i]) return true;
         }
-        
-        return minList;
+        return false;
     }
 }
