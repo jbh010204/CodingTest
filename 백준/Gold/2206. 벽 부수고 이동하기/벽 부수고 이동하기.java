b16628f09babc061e8d17516a230c72167010cb4
장보형 <95080710+jbh010204@users.noreply.github.com>
2026-02-25 18:41:34 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 648 ms, Memory: 109452 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,102 +1,74 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-/**
- * Gold 3: 벽 부수고 이동하기
- * 이것도 말이 되고픈 원숭이 문제처럼 3차원으로 풀면 되지 않을까
- */
-public class Main {
-    static int ROW, COL;
-    static int[][] gp;
-    static boolean visited[][][];
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    public static void main(String[] args) throws IOException {
-        input();
-        bfs();
+class Main {
+    static int N,M;
+    static int[][] arr;
+    static boolean[][][] visited;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int SPACE = 0;
+    static int WALL = 1;
+    static ArrayDeque<Node> dq = new ArrayDeque<>();
+    static class Node{
+        int x; int y; int dist; int broke;
+        public Node(int x, int y, int dist, int broke){
+            this.x=x; 
+            this.y=y;
+            this.dist=dist;
+            this.broke=broke;
+        }
     }
-
-    static void input() throws IOException {
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        ROW = Integer.parseInt(st.nextToken());
-        COL = Integer.parseInt(st.nextToken());
 
-        gp = new int[ROW][COL];
-        visited = new boolean[ROW][COL][2];
+        //초기화
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        visited = new boolean[N][M][2];
 
-        for(int i=0; i<ROW; i++){
+        for(int i=0; i<N; i++){
             String str = br.readLine();
-            for(int j=0; j<COL; j++){
-                gp[i][j] = str.charAt(j) - '0';
+            for(int j=0; j<M; j++){
+                arr[i][j] = str.charAt(j) - '0';
             }
         }
+
+        visited[0][0][1] = true;
+        dq.offer(new Node(0,0,1,0));
+        System.out.println(bfs());
+        
     }
 
-    static void bfs(){
-        Queue<Node> q = new LinkedList<>();
-        visited[0][0][1] = true;
-        q.add(new Node(0,0,1,1));
+    static int bfs(){
+        while(!dq.isEmpty()){
+            Node curr = dq.pollFirst();
 
-
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-
-            //System.out.println("X: "+cur.x+" Y: "+cur.y);
-
-            if(cur.x == ROW-1 && cur.y == COL-1){
-                System.out.println(cur.dist);
-                return;
-            }
+            if(curr.x == N-1 & curr.y == M-1) return curr.dist;
 
             for(int i=0; i<4; i++){
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
+                int nx = curr.x + dx[i];
+                int ny = curr.y + dy[i];
 
-                if(nx < 0 || ny < 0 || nx >= ROW || ny >= COL) continue;
-                if(visited[nx][ny][cur.k]) continue;
-                if(gp[nx][ny] == 1) continue;
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 
-                visited[nx][ny][cur.k] = true;
-                q.add(new Node(nx,ny,cur.k, cur.dist + 1));
-            }
-
-            if(cur.k > 0){
-                for(int i=0; i<4; i++){
-                    int nx = cur.x + dx[i];
-                    int ny = cur.y + dy[i];
-                    int nk = cur.k -1;
-
-                    if(nx < 0 || ny < 0 || nx >= ROW || ny >= COL) continue;
-                    if(visited[nx][ny][nk]) continue;
-                    if(gp[nx][ny] != 1) continue;
-
-                    visited[nx][ny][nk] = true;
-                    q.add(new Node(nx,ny,nk, cur.dist + 1));
+                if(arr[nx][ny] == SPACE && !visited[nx][ny][curr.broke]){
+                    visited[nx][ny][curr.broke] = true;
+                    dq.offer(new Node(nx,ny, curr.dist + 1, curr.broke));
                 }
+                else if(arr[nx][ny] == WALL){
+                    if(curr.broke < 1 && !visited[nx][ny][curr.broke + 1]){
+                        visited[nx][ny][curr.broke + 1] = true;
+                        dq.offer(new Node(nx,ny, curr.dist + 1, curr.broke + 1));    
+                    }
+                }
+                
             }
         }
-
-        System.out.println(-1);
-    }
-
-    static class Node{
-        int x;
-        int y;
-        int k;
-        int dist;
-
-        Node(int x, int y, int k, int dist){
-            this.x = x;
-            this.y = y;
-            this.k = k;
-            this.dist = dist;
-        }
+        return -1;
     }
 }
